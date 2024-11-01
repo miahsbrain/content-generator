bc3f71e58c86ae9ff738c23f32c8646568da6987
miahsbrain <miahspremium@gmail.com>
2024-11-01 14:10:16 +0100

    payment gateway completed and information saved to database

--- app/dashboard/_components/PayPalSubscribeButton.tsx
+++ app/dashboard/_components/PayPalSubscribeButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
-// import { createSubscription } from '@/utils/Db';
+import { useUserContext } from '@/app/(context)/UserContext';
+import { createSubscription } from '@/utils/Db';
+import { useSubscription } from '@/app/(context)/SubscriptionContext';
 
 interface PayPalSubscribeButtonProps {
   onSuccess: () => void;
@@ -9,6 +11,13 @@ interface PayPalSubscribeButtonProps {
 
 export function PayPalSubscribeButton({ onSuccess, onError }: PayPalSubscribeButtonProps) {
     const [{ isResolved }] = usePayPalScriptReducer();
+	const { userEmail } = useUserContext()
+	const { refreshSubscription } = useSubscription()
+
+	const paymentSuccess = async(payer_id: string) => {
+		await createSubscription(userEmail || '', 'plus', payer_id)
+		await refreshSubscription(userEmail || '');
+	}
 
     // const handleSubscription = async (data, actions) => {
 	// 	return actions.order.create({
@@ -45,18 +54,19 @@ export function PayPalSubscribeButton({ onSuccess, onError }: PayPalSubscribeBut
 				throw new Error('Actions not defined at PayPal')
 			}
 			return actions.order.capture().then((details) => {
-				if (!details.payer) {
+				if (!details.payer || !details.payer.payer_id) {
 					throw new Error('There was a error with paypal response')
 				}
-				alert(`Transaction completed by ${details.payer.name?.given_name}`);
+				// alert(`Transaction completed by ${details.payer.name?.given_name}`);
 				// Add post-transaction handling here
+				paymentSuccess(details.payer.payer_id)
 				onSuccess()
 			});
 		}}
 		onError={(error) => {
 			console.error('PayPal Checkout Error:', error);
 			// alert('There was an error processing your payment. Please try again.');
-			onError('error')
+			onError(`${error}`)
 		}}
 	/>
   );
